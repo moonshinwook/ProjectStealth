e9963c9b68e1cd67e1de951f6a10a8400b308f6d
moonshinwook <msw4761@naver.com>
2026-08-20 16:57:24 +0900

    Day6
    In-Place(제자리) motion 구현을 위해
    Animation의 Pelvis, root의 retargeting Option에서 Animaition -> Skeleton으로 수정
    BS_Walk 블루프린트에 Transform (Modify) Bone 노드 추가하여 Split Struct pin하여 X Y 값을 0으로 고정시키고 Z축은 95로 기존 애니메이션이 가지는 값으로 고정하여 In-Place 변환 성공
    
    등반, 구르기에서는 In-Place변환시 모션이 어색해지기 때문에
    Walk, Jog와 다른 Action들에 대한 처리 방식을 다르게 해야함.
    현재 Alpha값을 Walk와 Jog에는 1.0, 다른 액션에는 Alpha값을 0으로 수정하는 것을 AI로부터 추천받음

--- Source/ProjectStealth/MyAnimInstance.cpp
+++ Source/ProjectStealth/MyAnimInstance.cpp
@@ -1,5 +1,46 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+
+void UMyAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	APawn* Pawn = TryGetPawnOwner();
+	 
+	if (IsValid(Pawn))
+	{
+		Character = Cast<ACharacter>(Pawn);
+
+		if (IsValid(Character))
+		{
+			CharacterMovement = Character->GetCharacterMovement();
+		}
+	}
+}
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(CharacterMovement))
+	{
+		FVector Velocity = CharacterMovement->Velocity;
+		float GroundSpeed = Velocity.Size2D();
+		FRotator Rotation = Character->GetActorRotation();
+		FVector UnrotateVector = Rotation.UnrotateVector(Velocity);
+		UnrotateVector.Normalize();
+
+		Vertical = UnrotateVector.X;
+		Horizontal = UnrotateVector.Y;
+		
+
+		auto Acceleration = CharacterMovement->GetCurrentAcceleration();
+
+		ShouldMove = GroundSpeed > 0.1 && Acceleration != FVector::Zero();
+	}
+}
--- Source/ProjectStealth/MyAnimInstance.h
+++ Source/ProjectStealth/MyAnimInstance.h
@@ -14,6 +14,20 @@ class PROJECTSTEALTH_API UMyAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 private:
-	UPROPERTY(Category = "Character Movement", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
-	bool shouldMove;
+	UPROPERTY(Category = "Character Move", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	bool ShouldMove;
+	UPROPERTY(Category = "Character Move", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float Horizontal;
+	UPROPERTY(Category = "Character Move", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float Vertical;
+
+
+	UPROPERTY(VisibleAnywhere)
+	class ACharacter* Character;
+	UPROPERTY(VisibleAnywhere)
+	class UCharacterMovementComponent* CharacterMovement;
+
+public:
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 };
